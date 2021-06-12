eb8b2e540ec822cb9baff59c4d541ae283ed1fbc
summpot <Summpot@outlook.com>
2021-06-12 12:49:58 +0800

    Initial commit.

---
+++ Pixeval/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Pixeval
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App(MainWindow mainWindow)
+        {
+            MainWindow = mainWindow;
+        }
+        public new int Run()
+        {
+            return Run(MainWindow);
+        }
+    }
+}
---
+++ Pixeval/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Pixeval
+{
+    class Program
+    {
+        private static ServiceProvider _serviceProvider;
+
+        [STAThread]
+        public static void Main()
+        {
+            var serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+            _serviceProvider = serviceCollection.BuildServiceProvider();
+            var app = _serviceProvider.GetRequiredService<App>();
+            app.InitializeComponent();
+            _ = app.Run();
+        }
+
+        private static void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<MainWindow>();
+            services.AddSingleton<App>();
+        }
+    }
+}
